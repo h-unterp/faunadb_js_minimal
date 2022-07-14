55b51e9742b21b425c4dfafdee984a9c458a49a2
Hunter Peress <772917+h-unterp@users.noreply.github.com>
2022-07-14 08:49:15 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import faunadb from "faunadb";
+import "dotenv/config";
+const q = faunadb.query;
+const { Let, Do, Var } = q;
+
+const runMe = function () {
+  return Do(
+    Let(
+      {
+        thing1: 1,
+      },
+      Second(Var("thing1"))
+    )
+  );
+};
+
+var client = new faunadb.Client({
+  secret: process.env.FAUNA_SECRET,
+  domain: "db.us.fauna.com",
+});
+
+client.query(runMe).then((response) => console.log(response));
---
+++ readme.md
@@ -0,0 +1,8 @@
+#The bare minimum of a skeleton to test fuanadb with javascript
+
+```
+npm install
+node main.js
+...or
+node function.js
+```
