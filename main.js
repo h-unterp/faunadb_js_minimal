0d9526c2604d619ef810e6cd6f625e4432ce2b06
Hunter Peress <772917+h-unterp@users.noreply.github.com>
2022-07-14 09:06:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,16 @@ const q = faunadb.query;
 const { Let, Do, Var } = q;
 
 const runMe = function () {
-  return Do(
-    Let(
-      {
-        thing1: 1,
-      },
-      Second(Var("thing1"))
-    )
+  return Let(
+    {
+      thing1: 1,
+    },
+    Second(Var("thing1"))
   );
 };
 
 var client = new faunadb.Client({
-  secret: process.env.FAUNA_SECRET
+  secret: process.env.FAUNA_SECRET,
 });
 
 client.query(runMe).then((response) => console.log(response));
